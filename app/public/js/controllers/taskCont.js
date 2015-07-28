9b9754adab553a8092f59c2d93e57ba1303fe8df
Reza Afzalan <RAfzalan@gmail.com>
2015-07-28 10:56:58 +0430

    task utils client done

--- app/public/js/controllers/taskCont.js
+++ app/public/js/controllers/taskCont.js
@@ -2,6 +2,8 @@
 
 dashboardApp.controller('taskCont', function ($scope, itRequestService) {
     $scope.tasksclass = "glyphicon-ok";
+    $scope.newtask = '';
+    var selectedtaskid = -1;
     $scope.taskitemclick = function (id) {
         if ($scope.tasksclass === "glyphicon-ok") {
           $scope.tasks[id].selected = true;
@@ -11,10 +13,22 @@ dashboardApp.controller('taskCont', function ($scope, itRequestService) {
             $scope.tasks.splice(id,1);
           } else {
             if ($scope.tasksclass === "glyphicon-pencil") {
+              $scope.selectedtask = $scope.tasks[id].name;
+              selectedtaskid = id;
               $('#editTaskModal').modal('show');
             }
           }
         }
+    };
+    $scope.updatetaskname = function() {
+        $scope.tasks[selectedtaskid].name = $scope.selectedtask;
+        selectedtaskid = -1;
+        $('#editTaskModal').modal('hide');
+    };
+    $scope.addnewtask = function() {
+        $scope.tasks.push({ name : $scope.newtask });
+        selectedtaskid = -1;
+        $('#addTaskModal').modal('hide');
     };
     $scope.selectedtaskitemclick = function (id) {
         $scope.tasks[id].selected = false;
